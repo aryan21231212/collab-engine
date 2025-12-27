b307f496bf7ec968b8330ee734bad58f45f0fab6
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-12-27 22:50:46 +0530

    phase 1 done

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+let documentText = ""; // shared state
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // Send current document to new user
+  socket.emit("document:init", documentText);
+
+  // Receive text updates
+  socket.on("document:update", (newText) => {
+    documentText = newText;
+    socket.broadcast.emit("document:update", newText);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
